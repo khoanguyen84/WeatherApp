dab1f3db3da2985cd984f985ec82752b060230c7
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-07-21 13:45:11 +0700

    fix bug

--- weather.js
+++ weather.js
@@ -12,20 +12,21 @@ const sunset = document.querySelector('.sunset');
 const humidity = document.querySelector('.humidity>span');
 const windSpeed = document.querySelector('.wind-speed>span');
 
-function getWeather(){
+function getWeather() {
     let city = document.querySelector(".search-input").value || "Huế";
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}&lang=vi&units=metric`)
-      .then(async function(res){
-          let data = await res.json();
-          cityName.innerHTML = data.name ?? default_value;
-          weatherState.innerHTML = data.weather[0].description ?? default_value;
-          weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` ?? default_value;
-          weatherDegree.innerHTML = Math.round(data.main.temp) ?? default_value;
-          sunrise.innerHTML = moment.unix(data.sys.sunrise).format("HH:mm") ?? default_value;
-          sunset.innerHTML = moment.unix(data.sys.sunset).format("HH:mm") ?? default_value;
-          humidity.innerHTML = data.main.humidity ?? default_value;
-          windSpeed.innerHTML = (data.wind.speed *3.6).toFixed(1) ?? default_value;
-      })
-  }
+        .then(async function (res) {
+            let data = await res.json();
+            cityName.innerHTML = data.cod == '200' ? data.name : default_value;
+            weatherState.innerHTML = data.cod == '200' ? data.weather[0].description : default_value;
+            weatherIcon.src = data.cod == '200' ? `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` : 'http://openweathermap.org/img/wn/03d@2x.png';
+            weatherDegree.innerHTML = data.cod == '200' ? Math.round(data.main.temp) : default_value;
+            sunrise.innerHTML = data.cod == '200' ? moment.unix(data.sys.sunrise).format("HH:mm") : default_value;
+            sunset.innerHTML = data.cod == '200' ? moment.unix(data.sys.sunset).format("HH:mm") : default_value;
+            humidity.innerHTML = data.cod == '200' ? data.main.humidity : default_value;
+            windSpeed.innerHTML = data.cod == '200' ? (data.wind.speed * 3.6).toFixed(1) : default_value;
 
-  getWeather();
+        })
+}
+
+getWeather();
